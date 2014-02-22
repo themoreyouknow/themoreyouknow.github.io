5fb23576762562d482d3b3191667fcc9e020e265
bleader <bleader@ratonland.org>
2014-02-22 18:49:19 +0100

    enable disqus comments
    - add comments 'tmyk' account as disqus comments
    - regenerate to add comments

--- conf.py
+++ conf.py
@@ -30,7 +30,7 @@ website = 'http://127.0.0.1/blog/html/'
 # **************************************************************
 
 # Add your Disqus shortname to enable comments powered by Disqus
-disqus_shortname = None
+disqus_shortname = 'tmyk'
 
 # Change your favicon (new favicon goes in _static directory)
 html_favicon = 'tinkerer.ico'
